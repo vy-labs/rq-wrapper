909cf4dc2e130aa6af7c22035ca8ec9299148119
neelesh-nirmal <neelesh.nirmal@synaptic.com>
2022-01-05 21:27:43 +0530

    feat: custom job decorator with callback support

--- rq_wrapper/__init__.py
+++ rq_wrapper/__init__.py
@@ -1,9 +1,9 @@
 from rq import Retry
-from rq.decorators import job
 from rq.job import Job
 
 from .rq_queue import Queue
 from .rq_worker import Worker
+from .rq_job import job
 
 __all__ = (
     'Queue',
