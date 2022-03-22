dd908f1ae4709c3775295c96c28dd7c8e2297107
Ankit Kumar <ankitkr511@gmail.com>
2022-03-22 11:39:38 +0530

    Fix: default environemnt

--- rq_wrapper/settings.py
+++ rq_wrapper/settings.py
@@ -3,5 +3,5 @@ import os
 app_name = os.getenv('APP_NAME') or 'Unknown'
 app_type = os.getenv('APP_TYPE') or 'RqWorker'
 log_level = os.getenv('LOG_LEVEL') or 'INFO'
-environment = os.getenv('ENVIRONMENT') or 'default'
+environment = os.getenv('ENVIRONMENT') or 'DEVELOPMENT'
 namespace = os.getenv('RQ_NAMESPACE') or os.getenv('JOB_ID') or 'default'
