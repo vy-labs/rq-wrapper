d0be6be9fa6079678fecc0fe99ba741946e5cb09
Ankit Kumar <ankitkr@users.noreply.github.com>
2022-03-22 11:56:04 +0530

    custom job decorator with callback support along with logging (#13)
    * Modify to log using structlog
    
    * Copy files from feat/nn/improved-job-decorator
    
    * Fix: default environemnt

--- rq_wrapper/rq_worker.py
+++ rq_wrapper/rq_worker.py
@@ -1,11 +1,25 @@
+import structlog
 from rq import Worker as RqWorker, get_current_connection
 from rq.compat import as_text
 from rq.worker import compact
+from structlog_wrapper.python import configure_struct_logging
 
-from rq_wrapper.settings import namespace
+from rq_wrapper.rq_queue import Queue
+from rq_wrapper.settings import namespace, app_name, app_type, environment, log_level
+
+configure_struct_logging(app_name, app_type, environment, log_level=log_level)
+logger = structlog.getLogger(__name__)
 
 
 class Worker(RqWorker):
+    queue_class = Queue
+
+    def perform_job(self, job, queue):
+        result = super(Worker, self).perform_job(job, queue)
+        task_duration = str((job.ended_at - job.started_at).total_seconds()) + 's'
+        logger.info("task_completed", task_id=job.id, task_name=job.func_name, task_duration=task_duration,
+                    task_status=job.get_status(refresh=False), queue=queue.name)
+        return result
 
     @classmethod
     def all(cls, connection=None, job_class=None, queue_class=None, queue=None, serializer=None):
